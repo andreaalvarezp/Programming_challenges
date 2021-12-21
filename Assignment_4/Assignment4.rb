411070dc6d141fe48e6a303122f2e19ea24938c5
Andrea Alvarez Perez <andrea.alvarezp@alumnos.upm.es>
2021-12-21 08:39:09 -0500

    Assignment 4 update

--- Assignment_4/Assignment4.rb
+++ Assignment_4/Assignment4.rb
@@ -17,6 +17,7 @@ input = ARGV
 # ref: "Choosing BLAST options for better detection of orthologs as reciprocal best hits." (Moreno-Hagelsieb G, Latimer K.)
 # https://www.ncbi.nlm.nih.gov/pubmed/18042555
 $evalue = 10**-6
+$coverage = 0.5
 
 puts "\n---------------------------------------------"
 puts "ASSIGNMENT 4"
@@ -24,18 +25,38 @@ puts "Searching for orthologues"
 puts "Andrea Álvarez Pérez"
 puts "---------------------------------------------\n"
 
-if input.length != 4
+if input.length != 2
     puts "Incorrect number of arguments"
-    puts "USAGE: $bioruby <filename1> <type filename1> <filename2> <type filename2>"
-    puts "'Type' could be either 'nucl' or 'prot'"
+    puts "USAGE: $bioruby Assignment4.rb <filename1> <filename2>"
     puts "---------------------------------------------\n";puts
     exit 1
 end
 
+# Function to guess the file type
+def file_type(file)
+  seq = []
+  puts "Opening file"
+  File.open(file, 'r').each do |line|
+    # Avoid first line
+    if line[0] == '>' 
+      next
+    else
+      # Transforms the sequence in a Bio::Sequence object
+      seq = Bio::Sequence.auto(line)
+      break
+    end 
+  end
+  type = seq.guess.to_s.split('::')[-1] # Returns 'AA' or 'NA'
+  if type == "AA"
+    type_file = type.gsub("AA", "prot")
+  elsif type == "NA"
+    type_file = type.gsub("NA", "nucl")
+  end
+  return(type_file)  
+end
+
 file1 = input[0]
-type_file1 = input[1]
-file2 = input[2]
-type_file2 = input[3]
+file2 = input[1]
 
 if File.exists?("orthologues_output.txt")
     File.delete("orthologues_output.txt")
@@ -49,6 +70,10 @@ output.puts "Orthologue pairs found between Arabidopsis and S. pombe";output.put
 output.puts "File 1 #{file1} ID\t----------\tFile 2 #{file2} ID"
 output.puts "----------------------------------------------------------"
 
+# File type: nucl or prot
+type_file1 = file_type(file1)
+type_file2 = file_type(file2)
+
 puts "\nFile 1: #{file1}";puts"\tType: #{type_file1}"
 puts "File 2: #{file2}";puts"\tType: #{type_file2}"
 puts "---------------------------------------------\n"
@@ -57,28 +82,29 @@ file1_db = file1.to_s.split(".")[0]+"_db"
 file2_db = file2.to_s.split(".")[0]+"_db"
 
 # We create the databases
+system("rm -r Databases")
 system("mkdir Databases")
 system("makeblastdb -in '#{file1}' -dbtype #{type_file1} -out ./Databases/#{file1_db.to_s}") 
 system("makeblastdb -in '#{file2}' -dbtype #{type_file2} -out ./Databases/#{file2_db.to_s}")
 
 # Depending on the seq type of the filesm we perform a type of blast
-if type_file1 == 'nucl' and type_file2 == 'nucl'
+if type_file1 == "nucl" and type_file2 == "nucl"
     factory_f1 = Bio::Blast.local('blastn', "./Databases/#{file1_db.to_s}")
     factory_f2 = Bio::Blast.local('blastn', "./Databases/#{file2_db.to_s}")
 
-elsif type_file1 == 'nucl' and type_file2 == 'prot' 
+elsif type_file1 == "nucl" and type_file2 == "prot" 
     factory_f1 = Bio::Blast.local('tblastn', "./Databases/#{file1_db.to_s}")
     factory_f2 = Bio::Blast.local('blastx', "./Databases/#{file2_db.to_s}")
 
-elsif type_file1 == 'prot' and type_file2 == 'nucl'
+elsif type_file1 == "prot" and type_file2 == "nucl"
     factory_f1 = Bio::Blast.local('blastx', "./Databases/#{file1_db.to_s}")
     factory_f2 = Bio::Blast.local('tblastn', "./Databases/#{file2_db.to_s}")
 
-elsif type_file1 == 'prot' and type_file2 == 'prot' 
+elsif type_file1 == "prot" and type_file2 == "prot"
     factory_f1 = Bio::Blast.local('blastp', "./Databases/#{file1_db.to_s}")
     factory_f2 = Bio::Blast.local('blastp', "./Databases/#{file2_db.to_s}")
-
 end
+
 puts "\nRunning #{factory_f1.program} with file 1 and #{factory_f2.program} with file 2..."
 
 # We create Bio::FastaFormat objects for each of the files
@@ -93,8 +119,6 @@ end
 
 # Count number orthologues found
 sum = 0
-evalue = 10**-6
-
 puts "Searching for reciprocal best hits..."
 fasta_file1.each do |seq_f1| # We iterate over each sequence in the file1 
   # We extract the ID of each sequence in file1
@@ -103,28 +127,41 @@ fasta_file1.each do |seq_f1| # We iterate over each sequence in the file1
   report_f2 = factory_f2.query(seq_f1)
   
   # Only keep the BLAST result if it passes the evalue filter
-  if report_f2.hits[0] && report_f2.hits[0].evalue <= evalue  
-    # Extract the ID of each sequence in file2
-    file2_id = (report_f2.hits[0].definition.match(/(\w+\.\w+)|/)).to_s 
-    # Run the actual BLAST by querying the factory of file 1 with each of the sequences of the file2
-    # To get the sequence of each file2_id, we look at the hash we create previously where we linked each ID with its sequence
-    report_f1 = factory_f1.query(">#{file2_id}\n#{file2_hash[file2_id]}")
-    
-    # Only keep the BLAST result if it passes the evalue filter
-    if report_f1.hits[0] && report_f1.hits[0].evalue <= evalue  
-      # Store the ID that will match with the ID in file1
-      f1_match = (report_f1.hits[0].definition.match(/(\w+\.\w+)|/)).to_s 
-      # If the match and the file1_file ID match = reciprocal best hit 
-      if file1_id == f1_match
-        #puts "\t#{file1_id}\t\t----------\t\t#{file2_id}" 
-        output.puts "\t#{file1_id}\t\t\t----------\t\t#{file2_id}"
-        sum += 1
-        if sum%100==0
-            puts "Orthologues found: #{sum}"
-            puts "Resuming search..."
-        end      
-        
-      end
+  if (report_f2.hits[0])
+    if factory_f2.program == "blastx" 
+      cov_f2 = ((report_f2.hits[0].query_end.to_f - report_f2.hits[0].query_start.to_f)*3)/report_f2.hits[0].query_len.to_f
+    else
+      cov_f2 = (report_f2.hits[0].query_end.to_f - report_f2.hits[0].query_start.to_f)/report_f2.hits[0].query_len.to_f
+    end
+    if (report_f2.hits[0].evalue <= $evalue) and (cov_f2 >= $coverage) 
+      # Extract the ID of each sequence in file2
+      file2_id = (report_f2.hits[0].definition.match(/(\w+\.\w+)|/)).to_s
+      # Run the actual BLAST by querying the factory of file 1 with each of the sequences of the file2
+      # To get the sequence of each file2_id, we look at the hash we create previously where we linked each ID with its sequence
+      report_f1 = factory_f1.query(">#{file2_id}\n#{file2_hash[file2_id]}")
+      
+      # Only keep the BLAST result if it passes the evalue filter
+      if (report_f1.hits[0])
+        if factory_f1.program == "blastx"
+          cov_f1 = ((report_f1.hits[0].query_end.to_f - report_f1.hits[0].query_start.to_f)*3)/report_f1.hits[0].query_len.to_f
+        else
+          cov_f1 = (report_f1.hits[0].query_end.to_f - report_f1.hits[0].query_start.to_f)/report_f1.hits[0].query_len.to_f
+        end
+        if (report_f1.hits[0].evalue <= $evalue) and (cov_f1 >= $coverage) 
+          # Store the ID that will match with the ID in file1
+          f1_match = (report_f1.hits[0].definition.match(/(\w+\.\w+)|/)).to_s 
+          # If the match and the file1_file ID match = reciprocal best hit 
+          if file1_id == f1_match
+            #puts "\t#{file1_id}\t\t----------\t\t#{file2_id}" 
+            output.puts "\t#{file1_id}\t\t\t----------\t\t#{file2_id}"
+            sum += 1
+            if sum%100==0
+                puts "Orthologues found: #{sum}"
+                puts "Resuming search..."
+            end
+          end
+        end
+      end       
     end         
   end
 end
